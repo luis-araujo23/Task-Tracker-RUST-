4b2864f8037273e63cd8d254eb02bd84ffacb21f
luisaraujo <luisalfonsoaraujoleon@gmail.com>
2026-03-27 19:50:25 -0400

    user funcional

--- target/debug/deps/task_tracker.d
+++ target/debug/deps/task_tracker.d
@@ -1,10 +1,11 @@
-C:\Users\HP\Desktop\rust\Task-Tracker-RUST-\target\debug\deps\task_tracker.d: src\main.rs src\models\mod.rs src\models\task.rs src\storage\mod.rs src\storage\file_storage.rs src\commands\mod.rs src\commands\add.rs src\commands\update.rs src\commands\delete.rs src\commands\mark.rs src\commands\list.rs src\utils\mod.rs src\utils\time.rs
+C:\Users\HP\Desktop\rust\Task-Tracker-RUST-\target\debug\deps\task_tracker.d: src\main.rs src\models\mod.rs src\models\task.rs src\models\user.rs src\storage\mod.rs src\storage\file_storage.rs src\commands\mod.rs src\commands\add.rs src\commands\update.rs src\commands\delete.rs src\commands\mark.rs src\commands\list.rs src\commands\user.rs src\utils\mod.rs src\utils\time.rs
 
-C:\Users\HP\Desktop\rust\Task-Tracker-RUST-\target\debug\deps\task_tracker.exe: src\main.rs src\models\mod.rs src\models\task.rs src\storage\mod.rs src\storage\file_storage.rs src\commands\mod.rs src\commands\add.rs src\commands\update.rs src\commands\delete.rs src\commands\mark.rs src\commands\list.rs src\utils\mod.rs src\utils\time.rs
+C:\Users\HP\Desktop\rust\Task-Tracker-RUST-\target\debug\deps\task_tracker.exe: src\main.rs src\models\mod.rs src\models\task.rs src\models\user.rs src\storage\mod.rs src\storage\file_storage.rs src\commands\mod.rs src\commands\add.rs src\commands\update.rs src\commands\delete.rs src\commands\mark.rs src\commands\list.rs src\commands\user.rs src\utils\mod.rs src\utils\time.rs
 
 src\main.rs:
 src\models\mod.rs:
 src\models\task.rs:
+src\models\user.rs:
 src\storage\mod.rs:
 src\storage\file_storage.rs:
 src\commands\mod.rs:
@@ -13,5 +14,6 @@ src\commands\update.rs:
 src\commands\delete.rs:
 src\commands\mark.rs:
 src\commands\list.rs:
+src\commands\user.rs:
 src\utils\mod.rs:
 src\utils\time.rs:
